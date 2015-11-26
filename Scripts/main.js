ebc11c3ebaacf4847ba070a67123dd70f12fbf1c
SMaguina <smag23@gmail.com>
2015-11-25 20:34:03 -0800

    Custom js script
    Integrates to mapbox.js API

---
+++ Scripts/main.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function() {
+
+    L.mapbox.accessToken = 'pk.eyJ1Ijoic21hZzIzMCIsImEiOiJjaWd5azJiczEwdmxkd2NtNWhwZWhyNDh0In0.toAU2T3KTEZIEDfvfC9ONA';
+    L.mapbox.map('map', 'mapbox.streets')
+        .addControl(L.mapbox.geocoderControl('mapbox.places', {
+            keepOpen: true,
+            autocomplete: true
+        }));
+});
